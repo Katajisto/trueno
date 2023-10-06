77d24f1a6d060ba5e6679754fee543c04a4c8e7d
Tuomas Katajisto <t@ktj.st>
2023-10-06 20:01:31 +0300

    add logo and prepare 0.3.3

--- src/App.svelte
+++ src/App.svelte
@@ -33,7 +33,10 @@
 
 <div class="navbar bg-gray-200 shadow text-gray-700 font-bold flex py-2 px-2 items-center justify-between">
   <button class="hidden text-black bg-white rounded mr-5 p-2">Menu</button>
-  <h1 class="text-5xl">⚡ TRUENO</h1>
+  <div class="flex items-center">
+  <img class="h-14 mr-2" src="./logo.png" />
+  <h1 class="text-5xl">TRUENO</h1>
+  </div>
   <h1 class="text-2xl">{$treeSummary.join(" > ")}</h1>
   <h1 class="text-5xl mx-5 text-gray-400">V{conf.package.version}</h1>
 </div>
