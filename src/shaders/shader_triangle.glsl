a53727318afaa5e0eac72fe40d21e4d81d1d904d
Katajisto <t@ktj.st>
2025-05-01 17:35:03 +0300

    working textures and events

--- src/shaders/shader_triangle.glsl
+++ src/shaders/shader_triangle.glsl
@@ -4,20 +4,34 @@ in vec4 color0;
 in vec4 uv;
 
 out vec4 color;
+out vec4 texcoord;
 
 void main() {
     gl_Position = position;
     color = color0;
-    color.x += 0.00000001 * uv.x;
+    texcoord = uv;
 }
 @end
 
 @fs fs
 in vec4 color;
+in vec4 texcoord;
 out vec4 frag_color;
 
+layout(binding = 0) uniform texture2D tex;
+layout(binding = 0) uniform sampler smp;
+
+bool is_near(float a, float b) {
+    return abs(a-b) < 0.01;
+}
+
 void main() {
-    frag_color = color;
+    if(is_near(texcoord.x, -4) && is_near(texcoord.y, -2)) {
+        frag_color = color;
+    } else {
+        vec4 sampled = texture(sampler2D(tex, smp), texcoord.xy) * color;
+        frag_color = sampled;
+    }
 }
 @end
 
