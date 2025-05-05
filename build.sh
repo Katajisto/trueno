513ead8ac6f77bdeaecef4b3aafe4ec2144c5290
Katajisto <t@ktj.st>
2025-05-05 22:29:30 +0300

    start working on ray casting

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+set -e
 cd src/shaders/
 ./compile_shaders.sh
 cd ..
 cd ..
 /home/katajisto/bin/jai/bin/jai-linux -x64 first.jai
+./first &
 /home/katajisto/bin/jai/bin/jai-linux first.jai - wasm
--- src/shaders/shader_trixel.glsl
+++ src/shaders/shader_trixel.glsl
@@ -1,19 +1,20 @@
 @vs vs_trixel
 
 in vec4 position;
-// in vec4 inst;
+in vec4 inst;
+in vec4 inst_col;
 
 layout(binding=0) uniform vs_params {
     mat4 mvp;
 };
 
+
 out vec4 color;
 
 void main() {
-    // vec3 instancepos = inst.xyz;
-    // gl_Position = mvp * (vec4(position.xyz + instancepos, 1.0));
-    gl_Position = mvp * position;
-    color = vec4(1.0, 0.0, 0.0, 1.0);
+    vec3 instancepos = inst.xyz;
+    gl_Position = mvp * (vec4(position.xyz + instancepos, 1.0));
+    color = inst_col;
 }
 @end
 
