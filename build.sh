d8ad5acf7778e2606e0dfec93425eaa14e6e81c3
katajisto <t@ktj.st>
2025-07-07 22:27:12 +0300

    Work on port of meshgen code

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ cd src/shaders/
 ./compile_shaders.sh
 cd ..
 cd ..
-/home/katajisto/bin/jai/bin/jai-linux -x64 first.jai
-./first &
-/home/katajisto/bin/jai/bin/jai-linux first.jai - wasm
+jai -x64 first.jai
+./first
