5d017f0a2bfb84636236a342d71c7fa123593928
katajisto <t@ktj.st>
2025-07-10 23:57:14 +0300

    Work on level editor and trile rendering in level editor

---
+++ src/shaders/shader_trile.glsl
@@ -0,0 +1,32 @@
+@module bla
+
+@vs vs_trile
+
+in vec4 position;
+in vec4 normal;
+
+layout(binding=0) uniform trile_vs_params {
+    mat4 mvp;
+};
+
+
+out vec4 fnormal;
+
+void main() {
+    gl_Position = mvp * vec4(position.xyz, 1.0);
+    fnormal = normal;
+}
+@end
+
+@fs fs_trile
+
+in vec4 fnormal;
+out vec4 frag_color;
+
+
+void main() {
+    frag_color = vec4(fnormal.xyz, 1.0);
+}
+@end
+
+@program trile vs_trile fs_trile
