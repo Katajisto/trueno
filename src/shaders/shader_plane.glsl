e6f64d023824c23bb28d5cb371c1f132bbf35538
Tuomas Katajisto <tuomas.katajisto@Tuomas-MacBook-Pro.local>
2025-09-26 22:08:42 +0300

    imrpove grass

--- src/shaders/shader_plane.glsl
+++ src/shaders/shader_plane.glsl
@@ -1,5 +1,6 @@
 @vs vs_plane
 
+
 in vec4 position;
 
 layout(binding=0) uniform plane_vs_params {
@@ -12,7 +13,7 @@ out flat int idx;
 
 void main() {
     vec3 multisize = vec3(position.xyz * 1000.0);
-    gl_Position = mvp * (vec4(multisize.x, 0.0 + float(gl_InstanceIndex) * 0.01, multisize.z, 1.0));
+    gl_Position = mvp * (vec4(multisize.x, 0.0 + float(gl_InstanceIndex) * 0.002, multisize.z, 1.0));
     pos = position;
     idx = gl_InstanceIndex;
 }
@@ -58,25 +59,45 @@ layout(binding=1) uniform plane_world_config {
     float time;
 };
 
+#define hash(p)  fract(sin(dot(p, vec2(11.9898, 78.233))) * 43758.5453)
+
+float B(vec2 U) {          
+    float v =  hash( U + vec2(-1, 0) )
+             + hash( U + vec2( 1, 0) )
+             + hash( U + vec2( 0, 1) )
+             + hash( U + vec2( 0,-1) ); 
+    return  hash(U) - v/4.  + .5;
+}
+
 void main() {
     
     if(planeType == 1) {
         frag_color = vec4(0.0, 0.0, 1.0, 1.0);
     } else {
-        float density = 100000.0;
-        vec2 uv = round(pos.xz * density);
-        float distanceFromCenter = length(uv - (pos.xz * density));
+        float density = 80000.0;
+        vec2 densifiedCoordinate = pos.xz * density;
+        densifiedCoordinate.x += sin(densifiedCoordinate.y);
+        densifiedCoordinate.y += sin(densifiedCoordinate.x);
+        vec2 ruohokeskus = round(densifiedCoordinate);
+        float distanceFromCenter = length(ruohokeskus - (densifiedCoordinate));
         
-        float rand = hash12(uv);
-        float h = (1.0 / 16.0) * idx;
-
-        float thickness = 0.5;
         
-        if((rand - h) * thickness < distanceFromCenter) {
+        float rand = B(ruohokeskus) + sin(pos.x) * 0.4;
+        if(idx > 0 && rand < 0.2) {
             discard;   
         }
+        
+        float h = (1.0 / 64.0) * idx;
 
-        frag_color = vec4(0.0, rand, 0.0, 1.0);
+        float thickness = 0.5;
+
+        
+        if(idx > 0 && (rand - h) * thickness < distanceFromCenter) {
+            discard;   
+        } else {
+            frag_color = vec4(0.0, min(1.0, h + 0.1), 0.0, 1.0);
+        }
+
     }
 }
 @end
