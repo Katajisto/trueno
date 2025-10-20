e3fa3031fa10134a78d5d9314971bfe893b546f5
Katajisto <t@ktj.st>
2025-10-20 20:18:21 +0300

    ssao works

--- src/shaders/shader_ssao.glsl
+++ src/shaders/shader_ssao.glsl
@@ -29,7 +29,7 @@ out vec4 out_color;
 void main() {
     vec3 frag_pos = texture(sampler2D(g_position, ssao_smp), quad_uv).xyz;
     vec3 normal = normalize(texture(sampler2D(g_normal, ssao_smp), quad_uv).rgb);
-    vec2 noise_scale = vec2(1280.0/4.0, 720.0/4.0); // @Incomplete: get screen size
+    vec2 noise_scale = vec2(1920.0/4.0, 1080.0/4.0); // @Incomplete: get screen size
     vec3 random_vec = normalize(texture(sampler2D(tex_noise, ssao_smp), quad_uv * noise_scale).xyz);
 
     vec3 tangent = normalize(random_vec - normal * dot(random_vec, normal));
@@ -37,22 +37,20 @@ void main() {
     mat3 tbn = mat3(tangent, bitangent, normal);
 
     float occlusion = 0.0;
-    for (int i = 0; i < 64; ++i) {
+    for(int i = 0; i < 64; i++) {
         vec3 sample_pos = tbn * samples[i].xyz;
-        sample_pos = frag_pos + sample_pos;
+        sample_pos = frag_pos + sample_pos * 0.2;
 
         vec4 offset = vec4(sample_pos, 1.0);
         offset = projection * offset;
-        offset.xy /= offset.w;
-        offset.xy = offset.xy * 0.5 + 0.5;
+        offset.xyz /= offset.w;
+        offset.xyz = offset.xyz * 0.5 + 0.5;
 
         float sample_depth = texture(sampler2D(g_position, ssao_smp), offset.xy).z;
-        float range_check = smoothstep(0.0, 1.0, 1.0 - (frag_pos.z - sample_depth));
-        occlusion += (sample_depth >= sample_pos.z ? 1.0 : 0.0) * range_check;
+        occlusion += (sample_depth >= sample_pos.z ? 1.0 : 0.0);
     }
-
     occlusion = 1.0 - (occlusion / 64.0);
-    out_color = vec4(occlusion, occlusion, occlusion, 1.0);
+    out_color = vec4(vec3(occlusion), 1.0);
 }
 @end
 
